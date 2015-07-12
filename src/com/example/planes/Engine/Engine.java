bdddcaefd36efb4966673412560b5f483cd69e8e
iskandarov-egor <iskandarov_egor@mail.ru>
2015-07-12 15:16:36 +0300

    some comments

--- src/com/example/planes/Engine/Engine.java
+++ src/com/example/planes/Engine/Engine.java
@@ -3,6 +3,8 @@ package com.example.planes.Engine;
 /**
  * Created by egor on 12.07.15.
  */
+
+
 public class Engine {
     static SceneImpl scene = new SceneImpl();
 
--- src/com/example/planes/Engine/Scene.java
+++ src/com/example/planes/Engine/Scene.java
@@ -13,17 +13,21 @@ public interface Scene {
 
     void setPhysicsFPS(int fps);
 
+    // Период повторения мира по горизонтали (в экранах). Если равен нулю, то мир не повторяется.
     void setHorizontalPeriod(float period);
 
     void zoom(float zoom);
 
     SceneObject createObject(float x, float y, ObjectGroup group);
+
+    // Создать объект, который не зависит от камеры, как бы приклеенный к камере
     Sticker createSticker(float x, float y);
 
     void setBackgroundColor(float r, float g, float b);
 
     void addCollisionListener(CollisionListener listener);
 
+    // для SetContentView в Activity
     View getView(Context context);
 
     void run();
